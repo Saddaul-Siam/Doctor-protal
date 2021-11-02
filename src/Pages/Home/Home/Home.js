ce5b64a40dc0239a15a780d09e2486c5ddad5764
saddaul-siam <saddadul.siam@gmail.com>
2021-11-02 20:22:13 +0600

    added banner option

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,15 +2,17 @@ import React from 'react';
 import Navigation from '../../Shared/Navigation/Navigation';
 import AppointmentBanner from '../AppointmentBanner/AppointmentBanner';
 import Banner from '../Banner/Banner';
+import BannerOption from '../BannerOption/BannerOption';
 import Services from '../Services/Services';
 
 const Home = () => {
   return (
     <div>
-      <Navigation></Navigation>
-      <Banner></Banner>
-      <Services></Services>
-      <AppointmentBanner></AppointmentBanner>
+      <Navigation />
+      <Banner />
+      <BannerOption />
+      <Services />
+      <AppointmentBanner />
     </div>
   );
 };
