fd7c4e05ffcc99497fa31619534e8583dbc8d8c4
saddaul-siam <saddadul.siam@gmail.com>
2021-11-02 23:29:36 +0600

    added modal

--- src/Pages/Appointment/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment/Appointment.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Navigation from '../../Shared/Navigation/Navigation';
+import AppointmentHeader from '../AppointmentHeader/AppointmentHeader';
+import AvailableAppointments from '../AvailableAppointments/AvailableAppointments';
 
 const Appointment = () => {
+  const [date, setDate] = React.useState(new Date());
   return (
     <div>
-      <h2>This is a appointment</h2>
+      <Navigation></Navigation>
+      <AppointmentHeader date={date} setDate={setDate}></AppointmentHeader>
+      <AvailableAppointments date={date}></AvailableAppointments>
     </div>
   );
 };
